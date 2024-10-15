a810f3e5929f8faf9a57edb4289f5e42206f22cc
AliBank <ali.bank198@gmail.com>
2024-10-15 20:21:33 +0300

    deneme

--- ShipCrewProject/src/app/app.component.ts
+++ ShipCrewProject/src/app/app.component.ts
@@ -1,28 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { MatTableModule } from '@angular/material/table';
+import { MatTableModule, MatTableDataSource } from '@angular/material/table';
 import { CrewService } from './services/crew.service';
 import { Crew } from './models/crew.model';
 import { Certificate } from './models/certificate.model';
+import { MatListModule } from '@angular/material/list'; // Import MatListModule here
+import { MatButtonModule } from '@angular/material/button'; // Import MatButtonModule if you're using mat-button
+import { MatCardModule } from '@angular/material/card'; // Import MatCardModule if using mat-card
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent implements OnInit{
   title = 'ShipCrewProject';
-
-  crewList: Crew[] = [];
+  crewList: Crew[] = []; 
   displayedColumns: string[] = ['id', 'name', 'lastName', 'role', 'dailyRate', 'actions'];
 
   constructor(private crewService: CrewService) {}
 
   ngOnInit(): void {
-    this.crewService.getCrewList().subscribe((crewList: Crew[]) => {
-      this.crewList = crewList;
+    this.crewService.getCrewList().subscribe((crew: Crew[]) => {
+      this.crewList = crew;
     });
   }
-
 }
--- ShipCrewProject/src/app/app.module.ts
+++ ShipCrewProject/src/app/app.module.ts
@@ -1,13 +1,18 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { MatTableModule } from '@angular/material/table';
+import { MatTableModule, MatTableDataSource } from '@angular/material/table';
 import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
+import { MatListModule } from '@angular/material/list'; // Import MatListModule here
+import { MatButtonModule } from '@angular/material/button'; // Import MatButtonModule if you're using mat-button
+import { MatCardModule } from '@angular/material/card'; // Import MatCardModule if using mat-card
+
 import { CrewService } from './services/crew.service';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, FormsModule, MatTableModule],
+  imports: [BrowserModule, FormsModule, MatTableModule, MatTableDataSource, BrowserAnimationsModule,MatListModule,MatButtonModule,MatCardModule],
   providers: [CrewService],
   bootstrap: [AppComponent]
 })
