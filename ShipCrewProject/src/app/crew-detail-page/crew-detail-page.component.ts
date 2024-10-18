aa1f4778008cdd036082a46988bcd8c47f1b082c
AliBank <ali.bank198@gmail.com>
2024-10-18 21:45:19 +0300

    crew detail page added

---
+++ ShipCrewProject/src/app/crew-detail-page/crew-detail-page.component.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { MatTableModule, MatTableDataSource } from '@angular/material/table';
+import { CrewService } from '../services/crew.service';
+import { Crew } from '../models/crew.model';
+import { Certificate } from '../models/certificate.model';
+import { MatListModule } from '@angular/material/list'; // Import MatListModule here
+import { MatButtonModule } from '@angular/material/button'; // Import MatButtonModule if you're using mat-button
+import { MatCardModule } from '@angular/material/card'; // Import MatCardModule if using mat-card
+
+@Component({
+  selector: 'crew-detail-page',
+  templateUrl: './crew-detail-page.component.html',
+  styleUrl: './crew-detail-page.component.scss'
+})
+export class CrewDetailPageComponent implements OnInit{
+  title = 'ShipCrewProject';
+
+  displayedColumns: string[] = [
+    'actions',
+    'id',
+    'firstName',
+    'lastName',
+    'nationality',
+    'title',
+    'daysOnBoard',
+    'dailyRate',
+    'currency',
+    'totalIncome'
+  ];
+  dataSource = new MatTableDataSource<Crew>();
+
+  constructor(private crewService: CrewService) {}
+
+  ngOnInit(): void {
+    this.getCrewList();
+  }
+
+  deleteCrewMember(id: number): void {
+    this.crewService.deleteCrew(id).subscribe(success => {
+      if (success) {
+        this.getCrewList();
+      }
+    });
+  }
+
+  getCrewList() {
+    this.crewService.getCrewList().subscribe((crewList: Crew[]) => {
+      this.dataSource.data = crewList;
+    });
+  }
+}
