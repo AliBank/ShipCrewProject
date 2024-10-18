5f121faf78153be196fa5ec5951520521107e3c8
AliBank <ali.bank198@gmail.com>
2024-10-18 22:30:44 +0300

    cleaning excess code

--- ShipCrewProject/src/app/crew-detail-page/crew-detail-page.component.ts
+++ ShipCrewProject/src/app/crew-detail-page/crew-detail-page.component.ts
@@ -9,48 +9,41 @@ import { MatButtonModule } from '@angular/material/button'; // Import MatButtonM
 import { MatCardModule } from '@angular/material/card'; // Import MatCardModule if using mat-card
 import { Inject } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'crew-detail-page',
   templateUrl: './crew-detail-page.component.html',
   styleUrl: './crew-detail-page.component.scss'
 })
-export class CrewDetailPageComponent implements OnInit{
+export class CrewDetailPageComponent implements OnInit {
   title = 'ShipCrewProject';
+  rCrew: any;
 
-  displayedColumns: string[] = [
-    'actions',
-    'id',
-    'firstName',
-    'lastName',
-    'nationality',
-    'title',
-    'daysOnBoard',
-    'dailyRate',
-    'currency',
-    'totalIncome'
-  ];
   dataSource = new MatTableDataSource<Crew>();
 
-  constructor(private crewService: CrewService , 
+  constructor(private crewService: CrewService,
+    private snackBar: MatSnackBar,
     public dialogRef: MatDialogRef<CrewDetailPageComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: any) {}
+    @Inject(MAT_DIALOG_DATA) public data: any) {
+    this.crewService.getCrewById(data.id).subscribe(result => {
+      if (result) {
+        this.rCrew = result;
+      }
+    },
+      error => { this.notify(error?.message, 'error', 5000); });
+  }
 
   ngOnInit(): void {
-    this.getCrewList();
+
   }
 
-  deleteCrewMember(id: number): void {
-    this.crewService.deleteCrew(id).subscribe(success => {
-      if (success) {
-        this.getCrewList();
-      }
-    });
-  }
 
-  getCrewList() {
-    this.crewService.getCrewList().subscribe((crewList: Crew[]) => {
-      this.dataSource.data = crewList;
+  // Notify method to show messages
+  notify(msg: string, type: 'success' | 'error', duration: number) {
+    this.snackBar.open(msg, 'Close', {
+      duration: duration,
+      panelClass: type === 'success' ? 'success-snackbar' : 'error-snackbar'
     });
   }
 }
